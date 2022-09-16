62de70199e60aeaafdbbf3751f91f003b4cbfccd
Aatif Rashid <aatif20@gmail.com>
2022-09-16 17:24:35 +0530

    error fixxedddd

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+var bodyParser = require('body-parser');
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 app.get('/getRoute',(req,res)=>{
 })
 app.post('/recieve-data',(req,res)=>{
+
+    response = {
+        first_name:req.body,
+        last_name:req.body
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+    console.log(req)
     if(req.body!== null ){
-        res.send("khali kyu bhej rha h")
+        res.end("khali kyu bhej rha h")
     }
     let data=req.body;
 
-    res.send("okay");
+    res.end("okay");
 })
 
 // takes raw json data and returns the gst no from it
 const cleanData=(data)=>{
-    const cleanedData;
+    const cleanedData="";
     return cleanedData
 }
 
